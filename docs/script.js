cd06ac9b63f729e73101d5ca6f4893c264fe5b1c
syouda0912 <80232090+syouda0912@users.noreply.github.com>
2021-05-14 16:23:46 +0900

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -36,6 +36,17 @@ peer.on('disconnected', function(){
 
 $('#make-call').submit(function(e){
     e.preventDefault();
+
+    if(localStream.getVideoTracks().length == 2){
+        // 既に２つのトラックが含まれる場合は、2つめのトラック（ScreenShare）を削除
+        localStream.removeTrack(localStream.getVideoTracks()[1]);
+    }
+
+    // キャンパス情報追加
+    var canvasVideo = document.getElementById("synthetic-canvas1");
+    var paintStream = canvasVideo.captureStream(25);
+    localStream.addTrack(paintStream.getVideoTracks()[0]);
+
     const call = peer.call($('#callto-id').val(), localStream);
     setupCallEventHandlers(call);
 });
@@ -57,8 +68,7 @@ function setupCallEventHandlers(call){
     existingCall = call;
 
     call.on('stream', function(stream){
-        var canvasstream = $('#canvasVideo').captureStream(25);
-        addVideo(call,canvasstream);
+        addVideo(call,stream);
         setupEndCallUI();
         $('#their-id').text(call.remoteId);
     });
@@ -69,7 +79,20 @@ function setupCallEventHandlers(call){
 }
 
 function addVideo(call,stream){
-    $('#their-video').get(0).srcObject = stream;
+    var _tracklengs = stream.getVideoTracks().length;
+    if( _tracklengs == 2){
+        var _peerVideo = new webkitMediaStream();
+        var _peerPaint = new webkitMediaStream();
+        // 作成した２つの空のMediaStream Objectに、
+        // それぞれビデオとキャプチャ用のTrackを追加する
+        _peerVideo.addTrack(stream.getVideoTracks()[0]);
+        _peerPaint.addTrack(stream.getVideoTracks()[1]);
+
+        $('#my-video').get(0).srcObject = _peerPaint;
+        $('#their-video').get(0).srcObject = _peerVideo;
+    }else{
+        $('#their-video').get(0).srcObject = stream;
+    }
 }
 
 function removeVideo(peerId){
